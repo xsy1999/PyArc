5266e8d64d8eead9d4585f4898a893a016890e2b
xsy1999 <42294450+xsy1999@users.noreply.github.com>
2024-03-13 18:35:09 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from pkg_resources import parse_requirements
+
+with open("requirements.txt", encoding="utf-8") as fp:
+    install_requires = [str(requirement) for requirement in parse_requirements(fp)]
+
+setup(
+    name="PyArc",
+    version="1.0.0",
+    author="Siyuan Xu",
+    author_email="2016302540149@whu.edu.cn",
+    description="A python Package for computing absorption"
+                " coefficients and radiative recombination rates in semiconductors",
+    license="MIT License",
+    url="https://github.com/xsy1999/PyArc/blob/main/README.md",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'test = test.help:main'
+        ]
+    }
+)
